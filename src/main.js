a1c39bd2687ec242980c23824d2dff774c9e6533
FEDwxp <18911295210@163.com>
2018-09-02 15:13:06 +0800

    header修改

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+// 引人入css样式
+import './assets/css/index.css';
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
